5dc968b716e762bfa8b1c16f7d194017401809f8
gqq w541 ubuntu <gqq_w541_ubuntu@itu.edu>
2017-06-05 21:48:36 -0700

    add some logs to traceactivity.java

--- com.vogella.android.lifecycle.activity/src/com/vogella/android/lifecycle/activity/TracerActivity.java
+++ com.vogella.android.lifecycle.activity/src/com/vogella/android/lifecycle/activity/TracerActivity.java
@@ -4,48 +4,56 @@ import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class TracerActivity extends Activity {
-
+	String tag = this.getClass().getSimpleName();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.d(tag, "onCreate");
 		notify("onCreate");
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
+		Log.d(tag, "onPause");
 		notify("onPause");
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
+		Log.d(tag, "onResume");
 		notify("onResume");
 	}
 
 	@Override
 	protected void onStop() {
 		super.onStop();
+		Log.d(tag, "onStop");
 		notify("onStop");
 	}
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
+		Log.d(tag, "onDestroy");
 		notify("onDestroy");
 	}
 
 	@Override
 	protected void onRestoreInstanceState(Bundle savedInstanceState) {
 		super.onRestoreInstanceState(savedInstanceState);
+		Log.d(tag, "onRestoreInstanceState");
 		notify("onRestoreInstanceState");
 	}
 
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
+		Log.d(tag, "onSaveInstanceState");
 		notify("onSaveInstanceState");
 	}
 
